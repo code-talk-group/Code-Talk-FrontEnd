a51677cdc27a451de3991d06c467ec0ede6821f9
Jon Rice <revyolution1120@gmail.com>
2019-07-08 17:44:13 -0700

    Views and controller set up

--- CodeTalk/CodeTalk/Startup.cs
+++ CodeTalk/CodeTalk/Startup.cs
@@ -36,10 +36,10 @@ namespace CodeTalk
             });
 
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
