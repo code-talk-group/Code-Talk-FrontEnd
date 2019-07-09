ef54c1f6d6d25041a50c88eb5754674b7715fa79
Jon Rice <revyolution1120@gmail.com>
2019-07-09 08:44:15 -0700

    drop down for form type complete. form controller and model started

---
+++ CodeTalk/CodeTalk/Models/Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTalk.Models
+{
+    public class Form
+    {
+        public FormType FormType { get; set; }
+    }
+
+    public enum FormType
+    {
+        Function,
+        Variable,
+        IfStatement,
+        ForLoop
+    }
+}
