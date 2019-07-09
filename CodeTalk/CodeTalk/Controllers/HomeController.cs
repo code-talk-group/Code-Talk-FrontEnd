3fe936486bf6cd1e31cdf31634005b3c359285d5
Jon Rice <revyolution1120@gmail.com>
2019-07-09 11:40:44 -0700

    Switching between pages from drop down.

--- CodeTalk/CodeTalk/Controllers/HomeController.cs
+++ CodeTalk/CodeTalk/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CodeTalk.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,32 @@ namespace CodeTalk.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
       public  IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Form form)
+        {
+            switch (form.FormType)
+            {
+                case FormType.Function:
+                    return RedirectToAction("Function", "Forms");
+
+                case FormType.Variable:
+                    return RedirectToAction("Variable", "Forms");
+
+                case FormType.IfStatement:
+                    return RedirectToAction("IfStatement", "Forms");
+
+                case FormType.ForLoop:
+                    return RedirectToAction("ForLoop", "Forms");
+
+                default:
+                    return RedirectToAction("Index", "Home");
+            }
         }
     }
 }
