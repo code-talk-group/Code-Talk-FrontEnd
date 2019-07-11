aeb61e21191db83278d967a53ba8ac9cb92ac508
Jon Rice <revyolution1120@gmail.com>
2019-07-11 10:57:15 -0700

    Refactored for creating different forms. Sending json to api.

---
+++ CodeTalk/CodeTalk/Models/Variable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTalk.Models
+{
+    public class Variable
+    {
+        public string CodeName { get; set; }
+
+        public string MethodName { get; set; }
+
+        public string DataType { get; set; }
+
+        public string VariableName { get; set; }
+
+        public string VariableValue { get; set; }
+    }
+}
